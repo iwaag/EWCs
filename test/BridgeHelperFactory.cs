99cfe04618e60a730d72c6219ebc33bc12f9c27d
iwaag <eiji-of-empires@nifty.com>
2018-06-22 18:00:01 +0900

    add

---
+++ test/BridgeHelperFactory.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+namespace AGDev {
+	
+}
---
+++ test/ConfigurableBAnalyser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+namespace AGDev {
+	
+	public class FloatAnswerListenerBridge {
+		public AnswerListener<float> listener;
+		public FloatCallback callback;
+		public FloatAnswerListenerBridge(AnswerListener<float> _listener) {
+			listener = _listener;
+			callback = (float answer) => {
+				_listener.OnAnswerUpdate(answer);
+			};
+		}
+	}
+	
+}
